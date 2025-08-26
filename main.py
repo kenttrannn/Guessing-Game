dd6f63268742bcf2c72ae43caa824c379887e248
kenttrannn <kent.tran2219@gmail.com>
2025-08-26 11:54:09 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+#Kent Tran, Viktor Silaiev
+#August 26, 2025
+#Lab #1, Randomized Number Guessing Game
+import check_input
+import random
+def main():
+    print("- Guessing Game -")
+    randomizedNumber = random.randint(1, 100) #creates a random number and stores it under randomizedNumber
+    guessCount = 0
+    guess = 0
+    print(randomizedNumber)
+    
+    while (guess != randomizedNumber):#checking if the guess is right
+        guess = check_input.get_int_range("I'm thinking of a number. Make a guess (1-100): ", 1, 100)
+        if (guess < randomizedNumber):
+            print("Too low")
+            guessCount += 1
+        elif (guess > randomizedNumber):
+            print("Too high")
+            guessCount += 1
+        else:
+            guessCount += 1
+            if guessCount == 1:
+                print("Correct! You got it in " + str(guessCount) + " try")
+            else:
+                print("Correct! You got it in " + str(guessCount) + " tries")
+            
+
+main()
